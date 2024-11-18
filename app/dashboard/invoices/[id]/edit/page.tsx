7ce19afb4e49cd0b9aa98628d621abff4b0fe715
tesfaTMH <hagos.tesfam@gmail.com>
2024-11-18 12:39:52 +0200

    Add metadata to for SEO (Search Engine Optimization) and shareability

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,13 @@
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/edit-form";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Edit Invoices",
+};
 
 const page = async (props: { params: Promise<{ id: string }> }) => {
   const params = await props.params;
--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,12 @@
 import { fetchCustomers } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/create-form";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Create Invoices",
+};
 
 const CreateInvoicePage = async () => {
   const customers = await fetchCustomers();
